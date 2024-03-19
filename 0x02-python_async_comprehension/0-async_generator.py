3f9a0568aa6776e2bdfa9a237198da0ddfb2e286
Zemichael Yadeta <yetikimitabeba@gmail.com>
2024-03-19 16:35:41 +0300

    Return type annotation.

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,11 +5,13 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator:
+async def async_generator() -> Generator[float, None, None]:
     """A coroutine that yields a random number between 0and 10.
     Parameters: None
     Return: List[float]
     """
     for idx in range(10):
+        # suspend and sleep a moment
         await asyncio.sleep(1)
+        # yield a value to the caller
         yield random.uniform(0, 10)
