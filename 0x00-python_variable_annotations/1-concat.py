f11710301a94f4ec92539fef51d781ac21c9579c
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-03-29 14:50:27 +0300

    String concatenation function.

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Module with a type annotated string conacatenation function. """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Type annotated concatenation function. """
+    return str1 + str2
