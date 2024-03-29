3c4c3e4f28ac5add5a1a88616539d5ae0e95cd31
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-03-29 14:44:29 +0300

    Type annotated function.

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Module with type annotation function. """
+
+
+def add(a: float, b: float) -> float:
+    """ Type annotated function that accepts two float arguments
+    and returns their sum of float type.
+    """
+    return a + b
