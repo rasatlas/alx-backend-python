aab26997d3377026c651e4e05b0713d6ebd89527
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-03-29 14:57:48 +0300

    A floor function.

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module with a type annotated floor function. """
+import math
+
+
+def floor(n: float) -> int:
+    """ A function that returns the floor of a float argument. """
+    return math.floor(n)
