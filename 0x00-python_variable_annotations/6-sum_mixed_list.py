fd9827a4d2d7679218277112d02a30f0f65bd7c5
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-03-29 16:00:24 +0300

    List with mixed type elements.

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module defining type annotated sum_mixed_list function. """
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """ Function returning sume of hetrogeneous list elememnts. """
+    return sum(mxd_lst)
